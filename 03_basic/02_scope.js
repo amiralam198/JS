c62cea9314d643073ea42e43f94916b67f76728e
amiralam198 <157512945+amiralam198@users.noreply.github.com>
2025-06-01 16:59:04 +0530

    Global and local scope in JS

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,16 @@
+let a = 20;
+const b = 30
+var c = 40
+if(true){
+    let a = 50;
+    const b = 60
+    var c = 70
+}
+console.log(a);
+console.log(b);
+console.log(c);
+// Variables declared outside any function or in the global scope are known 
+// as global variables and can be accessed both inside and outside of functions
+
+// but "var" is exception it act as global inside the scope also
+
