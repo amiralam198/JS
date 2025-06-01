37592809bf81bfc5eb7bd91e76e1f5a0c6b16cf5
amiralam198 <157512945+amiralam198@users.noreply.github.com>
2025-06-01 17:22:43 +0530

    declaration of function and access before declaration

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -6,11 +6,48 @@ if(true){
     const b = 60
     var c = 70
 }
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
 // Variables declared outside any function or in the global scope are known 
 // as global variables and can be accessed both inside and outside of functions
 
 // but "var" is exception it act as global inside the scope also
 
+function one(){
+    const username = "amir"
+    function two(){
+        const webseite = "youtube"
+        // console.log(username);
+    }
+    // console.log(webseite) // error
+    two()
+}
+one()
+// ans -> hum andar ki function se bahar ko access kar sakte hai lekin bahar se andar ki nhi which means chote bade se icecream maang sakta h but bade chote se nhi
+ 
+// let us take an example of if else to understand the scope
+
+if(true){
+    const username = "amir"
+    if(username === "amir"){
+        const website = "youtube"
+        console.log(username+website)
+    }
+    // console.log(website)  // error
+}
+// console.log(username)  error
+
+// **** interesting ******
+
+console.log(addoOne(5))
+ function addoOne(nums1){
+    return nums1+1;
+ }
+// we can declare function like this and can access before the declaration of function
+
+console.log(addTwo(5))
+ const storeTwo = function addTwo(num1){
+    return num1+2;
+ }
+ // we can declare function like this but we can't access before the declaration of function
